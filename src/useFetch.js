f0d3a8ea63ff7b115df52e4bdb006f6895690c45
Lakhveer Bawa <bawa93lakhveer@gmail.com>
2021-04-23 18:18:46 +0530

    Feature: abort fetch request in case route is changed

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,25 +1,29 @@
-import {useState, useEffect} from "react";
+import {useEffect, useState} from "react";
 
 const useFetch = (url) => {
+    const abortCont = new AbortController();
     const [posts, setPosts] = useState([]);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
     useEffect(() => {
         setTimeout(() => {
-            fetch(url).then(response => {
+            fetch(url, {signal: abortCont.signal}).then(response => {
                 return response.json()
             }).then(data => {
                 setIsPending(false)
                 console.log(data)
                 setPosts(data.result.data)
             }).catch(error => {
-                setError(error)
+                if (error.name === 'AbortError') {
+                    console.log('abort error')
+                } else {
+                    setError(error)
+                }
             });
         }, 1000)
-
-
-    },[posts])
-    return { posts, isPending, error}
+        return () => abortCont.abort()
+    }, [posts])
+    return {posts, isPending, error}
 }
 export default useFetch
 
