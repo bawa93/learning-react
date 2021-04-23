795ce8c9e493be83bef549f4c31b009c0b4492d8
Lakhveer Bawa <bawa93lakhveer@gmail.com>
2021-04-23 08:27:09 +0530

    redux - basic setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from 'redux'
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,6 +12,35 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// actions 
+const increment = ()=> {
+  return {
+    type: "INCREMENT"
+  }
+}
+
+const decrement = ()=> {
+  return {
+    type: "DECREMENT"
+  }
+}
+
+// reducers 
+const Reducer = (state=0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state-1
+  }
+}
+// dispatch 
+
+const store = createStore(Reducer)
+
+store.subscribe(() => console.log(store.getState()))
+
+store.dispatch(increment())
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
