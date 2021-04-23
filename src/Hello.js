4eed40fbecdefc7a67286366a015d09646994b2b
Lakhveer Bawa <bawa93lakhveer@gmail.com>
2021-04-23 07:43:16 +0530

    style

--- src/Hello.js
+++ src/Hello.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 
 
 function Hello(props) {
+    const [isRed, setIsRed] = useState(false)
     const [helloCount, setHelloCount] = useState(0)
     
     const incrementHelloCount = () => {
-        setHelloCount(helloCount+1)
+        setHelloCount(helloCount + 1)
+        setIsRed(!isRed)
     }
-    return (<div>Hello { props.name} { helloCount} <button onClick={incrementHelloCount}>Increment Hello count</button></div>);
+    return (<div className={ isRed ? "red" : "blue"}>Hello { props.name} { helloCount} <button onClick={incrementHelloCount}>Increment Hello count</button></div>);
 }
 
 export default Hello;
